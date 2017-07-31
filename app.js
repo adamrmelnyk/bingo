362f38450b2349a0c28445c6daaa0b9c5ce2fe2a
arm <adam.melnyk@gmail.com>
2017-07-30 23:59:30 -0500

    Adding tests for the base functions

--- app.js
+++ app.js
@@ -1,36 +1,38 @@
 'use strict';
 const uuidv4 = require('uuid/v4');
 
-const range = (start, end = 15) => Array.apply(0, Array(end)).map((e, i) => (i + start));
+exports.range = (start, end = 15) => Array.apply(0, Array(end)).map((e, i) => (i + start));
 
-const randomColumn = start => {
-  array = range(start)
-  return shuffleArray(array).slice(0,5);
+exports.randomColumn = start => {
+  const array = exports.range(start);
+  return exports.shuffleArray(array).slice(0,5);
 };
 
-const shuffleArray = array => {
-  for (i = array.length - 1; i > 0; i--) {
-    j = Math.floor(Math.random() * (i + 1));
-    temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
+exports.shuffleArray = (array) => {
+  let clone = array.slice(0);
+  for (let i = clone.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = clone[i];
+    clone[i] = clone[j];
+    clone[j] = temp;
   };
-  return array
+  return clone
 };
 
-const standardBingoCard = () => {
-  nColumn = randomColumn(31);
+exports.standardBingoCard = () => {
+  const nColumn = exports.randomColumn(31);
   nColumn[2] = "Free";
   return {
-    b: randomColumn(1),
-    i: randomColumn(16),
+    b: exports.randomColumn(1),
+    i: exports.randomColumn(16),
     n: nColumn,
-    g: randomColumn(46),
-    o: randomColumn(61),
+    g: exports.randomColumn(46),
+    o: exports.randomColumn(61),
   };
 };
 
-const newBall = () => shuffleArray(range(1,75)).slice(0,1);
+// TODO: Should return the letter as well
+exports.newBall = () => exports.shuffleArray(exports.range(1,75)).slice(0,1)[0];
 
 exports.logical = (event) => {
   // TODO: read event and execute the correct function
